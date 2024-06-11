25ef552c6d76eab3b874a150f2c67e875f312f79
Anish Aritakula <anisharitakula@gmail.com>
2024-06-11 15:48:25 +1000

    added HTML linkage

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 from fastapi import FastAPI
 from code.agent import Stream_agent
+from fastapi.responses import HTMLResponse
 import uvicorn
 
 app=FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Welcome to the LLM maps agent"}
+    """Basic HTML response."""
+    body = (
+        "<html>"
+        "<body style='padding: 10px;'>"
+        "<h1>Welcome to the LLM maps&calculator API</h1>"
+        "<div>"
+        "Check the docs: <a href='/process_text'>here</a>"
+        "</div>"
+        "</body>"
+        "</html>"
+    )
+
+    return HTMLResponse(content=body)
 
 @app.post("/process_text")
 async def process_text(input_text):
