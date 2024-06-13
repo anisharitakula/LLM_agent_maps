3d99af44bd9f0873e692521cd18563317c27e066
Anish Aritakula <anisharitakula@gmail.com>
2024-06-13 19:53:31 +1000

    added PORT as a variable which Railway will insert

--- main.py
+++ main.py
@@ -40,4 +40,4 @@ async def query_text(input_text):
     return processed_text
 
 if __name__ == "__main__":
-    uvicorn.run(app,host="0.0.0.0",port=5000)
+    uvicorn.run(app,host="0.0.0.0",port=PORT)
