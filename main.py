62d8e5232c1970bad989d232fae542dd6b433ed7
Anish Aritakula <anisharitakula@gmail.com>
2024-06-13 15:01:02 +1000

    minor change in agent.py

--- main.py
+++ main.py
@@ -28,4 +28,4 @@ async def query_text(input_text):
     return processed_text
 
 if __name__ == "__main__":
-    uvicorn.run(app,host="localhost",port=8001)
+    uvicorn.run(app,host="localhost",port=8003)
